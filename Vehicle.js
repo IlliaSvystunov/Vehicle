7593c7a698c3d17131e67eff66b6fda646b820c8
IlliaSvystunov <illia.svystunov@teamdev.com>
2019-02-04 19:51:00 +0200

    Create different ships.

--- Vehicle.js
+++ Vehicle.js
@@ -1,6 +1,31 @@
 "use strict";
 
 /**
+ * Physical quantity that indicates the amount of occupied space.
+ * It is determined by three quantities: length, width, height.
+ */
+class Volume {
+
+    constructor(length, width, height) {
+
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    canPut(volume) {
+
+        return !!(this.length < volume.length
+        && this.width < volume.width
+        && this.height < volume.height)
+    }
+
+    calculateValue() {
+        return +(this.length * this.width * this.height);
+    }
+}
+
+/**
  * Class representing a vehicle.
  * Common vehicle has name, volume, weight, carrying volume, carrying weight.
  */
@@ -76,36 +101,30 @@ class Vehicle {
     }
 
     isVolumeMatches(vehicle) {
-        return vehicle.volume <= this.carryingVolume;
+        return this.volume.canPut(vehicle.volume);
     }
 }
 
 /**
  * Non-mechanical vehicle with 2 wheels and use pedaling as driving force.
  * Carries things only in baskets.
+ * Cannot carry another vehicle.
  */
 class Bicycle extends Vehicle {
 
     /**
      * Creates new bicycle with with specified:
-     * name, weight, volume, carryingVolume, carryingWeight, amountOfBaskets.
+     * name, weight, volume, carryingVolume, carryingWeight.
      * @param name
      * @param weight
      * @param volume
      * @param carryingWeight
      * @param carryingVolume
-     * @param amountOfBaskets
      * Default amount of baskets is 1.
      */
-    constructor(name, weight, volume, carryingWeight, carryingVolume, amountOfBaskets = 1) {
+    constructor(name, weight, volume, carryingWeight, carryingVolume) {
 
         super(name, weight, volume, carryingWeight, carryingVolume);
-        this._amountOfBaskets = amountOfBaskets;
-    }
-
-
-    get amountOfBaskets() {
-        return this._amountOfBaskets;
     }
 
     /**
@@ -114,8 +133,7 @@ class Bicycle extends Vehicle {
      * @returns {boolean}
      */
     canCarry(vehicle) {
-        return !!(this.isWeightMatches(vehicle)
-            && vehicle.volume <= this.carryingVolume/this.amountOfBaskets);
+        return false;
     }
 }
 
@@ -157,7 +175,7 @@ class Car extends Vehicle {
      */
     canCarry(vehicle) {
         return !!(super.isWeightMatches(vehicle)
-            && vehicle.volume <= this.baggageContainerVolume);
+            && this.baggageContainerVolume.canPut(vehicle.volume));
     }
 }
 
@@ -169,26 +187,16 @@ class AutoTrain extends Car {
 
     /**
      * Creates new auto train with with specified:
-     * name, weight, volume, amountOfTrailers, maxTrailerWeight, maxTrailerVolume.
+     * name, weight, volume, maxTrailerWeight, maxTrailerVolume.
      * @param name
      * @param weight
      * @param volume
-     * @param amountOfTrailers
      * @param maxTrailerWeight
      * @param maxTrailerVolume
      */
-    constructor(name, weight, volume, amountOfTrailers, maxTrailerWeight, maxTrailerVolume) {
+    constructor(name, weight, volume, maxTrailerWeight, maxTrailerVolume) {
 
-        let carryingWeight = amountOfTrailers * maxTrailerWeight;
-        let carryingVolume = amountOfTrailers * maxTrailerVolume;
-
-        super(name, weight, volume, carryingWeight, carryingVolume, 0);
-        this._amountOfTrailers = amountOfTrailers;
-    }
-
-
-    get amountOfTrailers() {
-        return this._amountOfTrailers;
+        super(name, weight, volume, maxTrailerWeight, maxTrailerVolume, 0);
     }
 
     /**
@@ -197,7 +205,71 @@ class AutoTrain extends Car {
      * @returns {boolean}
      */
     canCarry(vehicle) {
-        return !!(vehicle.weight <= this.carryingWeight/this.amountOfTrailers
-        && vehicle.volume <= this.carryingVolume/this.amountOfTrailers);
+        return !!(vehicle.weight <= this.carryingWeight
+        && this.carryingVolume.canPut(vehicle.volume));
+    }
+}
+
+/**
+ * Vehicle that moves on water surface.
+ */
+class Ship extends Vehicle {
+
+    constructor(name, weight, volume, carryingWeight, carryingVolume) {
+        super(name, weight, volume, carryingWeight, carryingVolume);
+    }
+
+    canCarry(vehicle) {
+        return super.canCarry(vehicle) && vehicle.volume.length < this.volume.length;
+    }
+}
+
+/**
+ * Ship that is usually used for fishing.
+ * Not intended for transportation vehicles.
+ */
+class FishBoat extends Ship {
+
+    constructor(name, weight, volume, carryingWeight, carryingVolume) {
+        super(name, weight, volume, carryingWeight, carryingVolume);
+    }
+
+    canCarry(vehicle) {
+        return false;
+    }
+}
+
+/**
+ * Ship for cargo transportation.
+ * Cargo is usually placed into containers.
+ */
+class SuperTanker extends Ship {
+
+    constructor(name, weight, volume, carryingWeight, carryingVolume, length, containerVolume) {
+        super(name, weight, volume, carryingWeight, carryingVolume, length);
+        this.containerVolume = containerVolume;
+    }
+
+    canCarry(vehicle) {
+        return !!(super.canCarry(vehicle) && this.containerVolume.canPut(vehicle.volume));
+    }
+}
+
+/**
+ * Very powerful military ship for transporting military vehicles.
+ * Can carry very large, wide and heavy things.
+ * All free space on board is used for carrying vehicles.
+ */
+class AircraftCarrier extends Ship {
+
+    constructor(name, weight, volume, carryingWeight, carryingVolume, length, width) {
+        super(name, weight, volume, carryingWeight, carryingVolume, length);
+        this.width = width;
+    }
+
+    canCarry(vehicle) {
+        return !!(vehicle.volume.length < this.volume.length/4
+        && vehicle.volume.width < this.volume.width/4
+        && vehicle.weight < this.weight/10);
     }
 }
